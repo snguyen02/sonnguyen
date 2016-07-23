d09edb92d14e53a8ef09c5fb2856ab6f3b93892e
son nguyen <sonnguyen02@gmail.com>
2016-07-23 15:30:36 +0700

    learn1

---
+++ src/main/java/com/mkyong/common/learn.java
@@ -0,0 +1,10 @@
+package com.mkyong.common;
+import java.util.*;
+class I4{
+  public static void main(String[] args){
+    Scanner s1 = new Scanner(System.in);
+    System.out.println("Enter your name");
+    String name = s1.nextLine();
+    System.out.println("Your name is " + name);
+  }
+}
